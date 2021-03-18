2d184fa55338b4b69875989b32e7d000bc3d9539
David Lin <david@narratiive.com>
2021-03-18 12:55:39 +1100

    first commit

---
+++ NarratiiveSDK/src/main/java/com/narratiive/narratiivesdk/NarratiiveAPI.kt
@@ -0,0 +1,94 @@
+package com.narratiive.narratiivesdk
+
+
+import android.content.Context
+import android.webkit.WebView
+import okhttp3.OkHttpClient
+import retrofit2.Callback
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.HeaderMap
+
+data class TokenInfo (
+    val host: String,
+    val hostKey: String,
+    val aaid: String?
+)
+
+data class HitInfo (
+    val token: String,
+    val host: String,
+    val hostKey: String,
+    val path: String?
+)
+
+data class ResponseData (
+    val token: String?,
+    val err: String?
+)
+
+interface ApiService {
+
+    @POST("tokens")
+    fun createToken(@Body data: TokenInfo, @HeaderMap headers: Map<String, String>): Call<ResponseData>
+
+
+    @POST("hits")
+    fun createHit(@Body data: HitInfo,  @HeaderMap headers: Map<String, String>): Call<ResponseData>
+}
+
+
+object ApiServiceBuilder {
+    //    private const val NARRATIIVE_API_URL = "https://collector.effectivemeasure.net/app/"
+    private const val NARRATIIVE_API_URL = "http://10.0.2.2:9002/app/"
+
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(NARRATIIVE_API_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    fun<T> buildService(service: Class<T>): T{
+        return retrofit.create(service)
+    }
+}
+
+
+class NarratiiveApiService(context: Context) {
+    private val retrofit = ApiServiceBuilder.buildService(ApiService::class.java)
+    private val userAgent =  WebView(context).settings.userAgentString
+    private val headers = hashMapOf("Content-Type" to "application/json", "User-Agent" to userAgent, "Accept" to "application/json")
+
+    fun createToken(data: TokenInfo, onResult: (ResponseData?) -> Unit){
+        retrofit.createToken(data, headers).enqueue(
+            object : Callback<ResponseData> {
+                override fun onFailure(call: Call<ResponseData>, t: Throwable) {
+                    println("NO...")
+                    onResult(null)
+                }
+                override fun onResponse( call: Call<ResponseData>, response: Response<ResponseData>) {
+                    onResult(response?.body())
+                }
+            }
+        )
+    }
+
+    fun createHit(data: HitInfo, onResult: (ResponseData?) -> Unit){
+        retrofit.createHit(data, headers).enqueue(
+            object : Callback<ResponseData> {
+                override fun onFailure(call: Call<ResponseData>, t: Throwable) {
+                    onResult(null)
+                }
+                override fun onResponse( call: Call<ResponseData>, response: Response<ResponseData>) {
+                    onResult(response?.body())
+                }
+            }
+        )
+    }
+}
---
+++ NarratiiveSDK/src/main/java/com/narratiive/narratiivesdk/NarratiiveSDK.kt
@@ -0,0 +1,86 @@
+package com.narratiive.narratiivesdk
+
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.android.gms.ads.identifier.AdvertisingIdClient
+
+object NarratiiveSDK {
+    private const val SHARED_PREF_FILE = "com.narratiive.sdk.sharepref"
+
+    private lateinit var context: Context
+    private lateinit var host: String
+    private lateinit var hostKey: String
+    private lateinit var sharedPref: SharedPreferences
+    private lateinit var apiService: NarratiiveApiService
+
+    private var aaid: String? = null
+    private var useAaid: Boolean = false
+    private var token: String? = null
+    private var isSending: Boolean = false
+
+
+    fun init(context: Context) {
+        this.context = context
+        this.apiService = NarratiiveApiService(context)
+        this.host = context.getString(R.string.narratiive_host)
+        this.hostKey = context.getString(R.string.narratiive_hostkey)
+        this.useAaid = context.getString(R.string.narratiive_use_aaid) == "1"
+        this.sharedPref = context.getSharedPreferences(SHARED_PREF_FILE, Context.MODE_PRIVATE)
+        this.token = this.sharedPref.getString("token", null)
+
+        if (this.useAaid) {
+            loadAdvertisingId()
+        }
+
+        if (this.token == null) {
+            createToken()
+        }
+    }
+
+    fun send(screenName: String?) {
+        this.createHit(screenName)
+    }
+
+    private fun loadAdvertisingId() {
+        Thread(Runnable {
+            try {
+                val info = AdvertisingIdClient.getAdvertisingIdInfo(this.context)
+                this.aaid = info.id
+            } catch (e: Exception) {
+                this.aaid = null
+            }
+        }).start()
+    }
+
+
+    private fun createToken() {
+        val tokenInfo = TokenInfo(host = this.host, hostKey = this.hostKey, aaid = this.aaid)
+
+        apiService.createToken(tokenInfo) {
+            this.token = it?.token
+        }
+    }
+
+    private fun createHit(path: String?) {
+        if (this.token != null && !isSending) {
+            val hitInfo =
+                HitInfo(host = this.host, hostKey = this.hostKey, token = this.token!!, path = path)
+            isSending = true
+            apiService.createHit(hitInfo) {
+                this.token = it?.token
+                saveToken()
+                isSending = false
+            }
+        }
+    }
+
+    private fun saveToken() {
+        this.token?.let {
+            with(this.sharedPref.edit()) {
+                putString("token", it)
+                apply()
+            }
+        }
+    }
+}
